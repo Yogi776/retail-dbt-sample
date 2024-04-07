8d746f5156125abc2ec69b61542acd01d98d3b5a
yogesh <yogesh.khangode@tmdc.io>
2024-04-07 22:04:43 +0530

    retail data product update

--- models/analysis/yoy_total_revenue.sql
+++ models/analysis/yoy_total_revenue.sql
@@ -1,23 +1,15 @@
-{{ config(materialized='view') }}
 
--- Generate the SQL with SECURITY DEFINER
-SELECT
-  date_format(order_date, '%Y') AS year,
-  count(DISTINCT customer_id) AS total_customers,
-  count(DISTINCT order_id) AS total_orders,
-  sum(order_amount) AS total_revenue
-FROM
-  icebase.retail.orders_enriched
-GROUP BY
-  1
-ORDER BY
-  1
 
-{% if target.name == 'trino' %}
-    -- Manually adjust the SQL to set SECURITY INVOKER
-    -- Replace SECURITY DEFINER with SECURITY INVOKER
-    -- This is a workaround since dbt doesn't directly support setting security type
-    {% set sql = this.sql.replace('SECURITY DEFINER', 'SECURITY INVOKER') %}
-{% endif %}
+{{ config(materialized='table') }}
 
-{{ sql }}
+select 
+  state, 
+  count(distinct customer_id) as total_customers,
+  count(distinct order_id) as total_orders, 
+  sum(order_amount) as total_revenue
+from 
+  icebase.retail.orders_enriched 
+where 
+  brand_name is not null 
+group by 1 
+order by 4 desc;
