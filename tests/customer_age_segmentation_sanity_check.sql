e7f888a236f052021dc66a328f572e9229686514
yogesh <yogesh.khangode@tmdc.io>
2024-04-08 11:53:35 +0530

    retail data product update

--- tests/customer_age_segmentation_sanity_check.sql
+++
@@ -1,9 +0,0 @@
-select
-    *
-from
-    {{ ref('customer_age_segmentation')}}
-where
-    age_bracket is null or
-    age_bracket not in ('Young Adults','Adults','Senior Citizens') or
-    total_customers < 0 or
-    total_orders < 0
--- tests/raw_orders_enriched_check.sql
+++
@@ -1,9 +0,0 @@
-select
-    *
-from
-    icebase.retail.orders_enriched
-where
-    customer_id is null or
-    order_id is null or
-    order_amount<0 or
-    order_amount is null
